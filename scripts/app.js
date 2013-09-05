997cf8012c6194ac394048331cfa9ad98a7cb8de
Hannes Probst <hannes@moped.com>
2013-09-05 18:12:05 +0200

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,73 @@
+/*
+
+http://stackoverflow.com/a/7557433/546030
+
+- requestRenderFrame
+*/
+
+var smoothScrollList = (function() {
+
+  var xhrGet = function(url, success, error) {
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE 8 and older
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhr.onreadystatechanged = function() {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          success(httpRequest.responseText);
+        } else {
+          error('There was a problem with the request.');
+        }
+      }
+    };
+    xhr.open('GET', url, true);
+    return xhr;
+  }
+
+  //http://jsperf.com/innerhtml-vs-dogfragment/10 TODO do own perf test
+  var createDomNode = function(item) {
+    /*
+      <li data-id="1">
+        <img src="img/default-image-1.png" alt="">
+        <p>John Doe</p>
+      </li>
+    */
+    var li = document.createElement('li');
+    
+    var img = document.createElement('img');
+    img.src = item.img;
+    li.appendChild(img);
+
+    var name = document.createElement('p');
+    name.nodeValue = item.name;
+    li.appendChild(name);
+
+    return li;
+  }
+
+  //http://stackoverflow.com/a/7557433/546030
+  isElementInViewport = function (el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document. documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document. documentElement.clientWidth) /*or $(window).width() */
+        );
+  }
+
+  var renderList = function() {
+    console.log(arguments);
+  }
+
+  var _init = function() {
+    xhrGet('scripts/data.json', renderList);
+  };
+
+  return {
+    init: _init
+  }
+})()
