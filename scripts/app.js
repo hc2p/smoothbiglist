fbab817583dbc169b32789b4c30527530c0baefd
Hannes Probst <hannes@moped.com>
2013-09-06 12:59:17 +0200

    fixed init; replaced initial naive render solution; no onscroll yet

--- scripts/app.js
+++ scripts/app.js
@@ -49,32 +49,86 @@ var smoothScrollList = (function() {
     return li;
   }
 
+  var appendDomNode = function(item) {
+    if (renderedElements[item.id]) return renderedElements[item.id];
+    var node = createDomNode(item);
+    listElement.appendChild(node);
+    renderedElements[item.id] = node;
+    return node;
+  }
+
   //http://stackoverflow.com/a/7557433/546030
   var isElementInViewport = function (el) {
     var rect = el.getBoundingClientRect();
 
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document. documentElement.clientHeight) && /*or $(window).height() */
-        rect.right <= (window.innerWidth || document. documentElement.clientWidth) /*or $(window).width() */
-        );
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document. documentElement.clientHeight) && /*or $(window).height() */
+      rect.right <= (window.innerWidth || document. documentElement.clientWidth) /*or $(window).width() */
+    );
   }
 
+  var getListItemHeight = (function() {
+    var listItemHeight;
+    return function(listData) {
+      if (listItemHeight) return listItemHeight;
+      
+      //render one first element
+      var node = appendDomNode(listData[0]);
+      listItemHeight = node.getBoundingClientRect().height;
+      return listItemHeight;
+    };
+  })()
+
+  var getViewportMax = (function() {
+    var viewportMax;
+    return function(listData) {
+      if (viewportMax) return viewportMax;
+      
+      var listItemHeight = getListItemHeight(listData);
+      var viewportOffset = getViewportOffset();
+      viewportMax = Math.round(viewportOffset.top + viewportOffset.height / listItemHeight);
+      return viewportMax + treshold;
+    };
+  })();
+  
   var renderList = function(listData) {
-    var ul = document.getElementsByTagName('ul')[0];
-    for(var i = 0, inView = true; i < 400, inView; i++) {
-      var node = createDomNode(listData[i]);
-      ul.appendChild(node);
-      if (! isElementInViewport(node)) inView = false;
+    
+    var viewportMax = getViewportMax(listData);
+
+    for(var i = 0; i < viewportMax; i++) {
+      appendDomNode(listData[i]);
     }
   }
 
-  var _init = function() {
-    var dataUrl = 'https://rawgithub.com/hc2p/smoothbiglist/master/scripts/data.json';
-    xhrJSONGet(dataUrl, renderList);
+  var getViewportOffset = function() {
+    var heightOfView = listElement.getBoundingClientRect().height;
+    var scrollTop = listElement.scrollTop;
+    return {
+      height: heightOfView,
+      top: scrollTop
+    }
+  };
 
-    //render a bunch incrementally check if in viewport
+  var listData;
+  var listElement;
+  var listItemHeight;
+  var treshold = 3; //elements to add regardless their visibility
+  var renderedElements = {};
+
+  var _init = function(options) {
+    if (typeof options !== 'object' || ! options.el) console.error('please initialize the list with an existing dom element');
+
+    listElement = options.el;
+
+    var dataUrl = 'https://rawgithub.com/hc2p/smoothbiglist/master/scripts/data.json';
+    xhrJSONGet(dataUrl, function(result) {
+      listData = result;
+      renderList(listData);
+    });
+
+    // render one element, take the height and compute a max of elements to append at once
     // after a certain treshold stop appending
     // onscroll add more
   };
